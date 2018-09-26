18f3147914c475067f0f541544db3d2f16f9ed0b
stradler <eamalakhov@gmail.com>
2018-09-26 17:27:12 +0300

    add redux packages

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import { composeWithDevTools } from "redux-devtools-extension";
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
